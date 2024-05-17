4a376fb3161d1079cf937b628f5eb68dcf4d0b90
Dymitr <dymitrkosow@gmail.com>
2024-05-17 09:08:08 +0200

    fix

--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ require('dotenv').config();
 
 const app = express();
 app.use(express.static('public'));
-app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cors({ origin:true, credentials: true }));
 
